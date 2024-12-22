c1db9979d8eb566ef09d25912d97ad78d19eb216
jbuk <mikellevolko@gmail.com>
2024-12-22 19:39:37 +0300

    v0.11.4

--- server_api.py
+++ server_api.py
@@ -208,4 +208,4 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug='false')
+    app.run(host='0.0.0.0', port=5000, debug='False')
