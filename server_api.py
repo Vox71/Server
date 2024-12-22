ea13fe334879297f4dd8958798a796005c1f354c
jbuk <mikellevolko@gmail.com>
2024-12-22 19:46:06 +0300

    v0.11.6

--- server_api.py
+++ server_api.py
@@ -201,9 +201,9 @@ def create_user():
     conn.close()
     return jsonify({'message': 'user created'})
 
-@app.route('/metrics')
-def metrics():
-    return generate_latest(), 200, {'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'}
+# @app.route('/metrics')
+# def metrics():
+#     return generate_latest(), 200, {'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug='False')
